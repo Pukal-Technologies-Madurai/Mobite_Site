a1b613d6c31355db0fe7cdbb8f65998bac2289b2
balagopinaath <balagopinaath@gmail.com>
2024-11-27 10:46:34 +0530

    chatbot

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,21 +3,21 @@ import { Helmet } from "react-helmet";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SocialDiv from "./SocialDiv";
+import Chatbot from "./Chatbot";
 
 const Layout = ({ children }) => {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Helmet>
-                <title>Mobite India</title>
-            </Helmet>
-            <Navbar />
-            <main className="flex-grow w-full">
-                {children}
-            </main>
-            <SocialDiv />
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Helmet>
+        <title>Mobite India</title>
+      </Helmet>
+      <Navbar />
+      <main className="flex-grow w-full">{children}</main>
+      <SocialDiv />
+      <Chatbot />
+      <Footer />
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
